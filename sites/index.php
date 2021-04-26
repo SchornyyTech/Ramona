79cb2683c6c03a84a09abe6db3871e5ec6532122
Schornyy <Schornstein.lukas@hotmail.com>
2021-04-26 22:49:18 +0200

    Content

--- sites/index.php
+++ sites/index.php
@@ -11,5 +11,23 @@
 
 <?php include("../sites/includes/navigation.php");?>
 
+<div class="content-container">
+    <div class="content content-left">
+        <img src="./img/Einzelgruppe.png" alt="Einzelkurse">
+        <p>Das ist ein Test</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet omnis reprehenderit placeat consequatur temporibus, maxime veritatis inventore impedit eum unde eaque, incidunt id consequuntur porro recusandae, natus voluptatum aperiam qui.</p>
+    </div>
+    <div class="content content-mid">
+        <img src="./img/Gruppenkurs.png" alt="Gruppenkurs">
+        <p>Das ist ein Test</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet omnis reprehenderit placeat consequatur temporibus, maxime veritatis inventore impedit eum unde eaque, incidunt id consequuntur porro recusandae, natus voluptatum aperiam qui.</p>
+    </div>
+    <div class="content content-right">
+        <img src="./img/Businesskurs.png" alt="Businesskurs">
+        <p>Das ist ein Test</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet omnis reprehenderit placeat consequatur temporibus, maxime veritatis inventore impedit eum unde eaque, incidunt id consequuntur porro recusandae, natus voluptatum aperiam qui.</p>
+    </div>
+</div>
+
 </body>
 </html>
