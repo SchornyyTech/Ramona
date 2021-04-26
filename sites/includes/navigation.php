f4cc2bf8f345829fd13397c5e24021eef9949584
Schornyy <Schornstein.lukas@hotmail.com>
2021-04-26 21:19:24 +0200

    no message

---
+++ sites/includes/navigation.php
@@ -0,0 +1,57 @@
+<nav class="nav">
+
+    <!--- Navigations Header --->
+    <div class="nav-header">
+        <div class="nav-brand">
+        <a href="#">
+            <img src="./img/Logo.png" alt="Logo">
+        </a>
+        </div>
+        <div class="nav-menu">
+            <ul class="nav-menu-list">
+                <li class="nav-menu-item">
+                    <a href="#">
+                    Home
+                    </a>
+                </li>
+                <li class="nav-menu-item">
+                    <a href="#">
+                    Leistungen
+                    </a>
+                </li>
+                <li class="nav-menu-item">
+                    <a href="#">
+                    Über&NonBreakingSpace;Uns
+                    </a>
+                </li>
+                <li class="nav-menu-item">
+                    <a href="#">
+                    Kontakt
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <!--- Navigations Footer --->
+    <div class="nav-footer">
+        <div class="nav-footer-left">
+            <ul>
+                <li><p><img src="./img/check.png" alt="check">Online Kurse</p></li>
+                <li><p><img src="./img/check.png" alt="check">Eintel oder Gruppen Kurse</p></li>
+                <li><p><img src="./img/check.png" alt="check">Businesskurse</p></li>
+            </ul>
+        </div>
+        <div class="nav-footer-right">
+            <p>Jetzt von überall verfügbar!</p>
+        </div>
+    </div>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave">
+        <path fill="#fff" 
+            fill-opacity="1" 
+            d="M0,192L80,202.7C160,213,320,235,480,213.3C640,192,800,128,960,128C1120,128,1280,192,1360,224L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
+        </path>
+    </svg>
+
+</nav>
---
+++ sites/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ramona Cobbs - Sprachen & mehr</title>
+    <link rel="stylesheet" href="./themes/style.css">
+</head>
+<body>
+
+<?php include("../sites/includes/navigation.php");?>
+
+</body>
+</html>
